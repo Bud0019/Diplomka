a26ba2833b90decfd464de789f3da7d007d42b52
Jakub Budos <bud0019@vsb.cz>
2015-11-17 12:15:12 +0100

    add database script

---
+++ AsteriskRoutingSystem/Database_script/Asterisk_DBScript.sql
@@ -0,0 +1,172 @@
+create database Asterisk_DB;
+
+use Asterisk_DB;
+
+create table Asterisks(
+id_Asterisk int primary key identity,
+name_Asterisk nvarchar(20) not null,
+prefix_Asterisk nvarchar(10) not null,
+ip_address nvarchar(50) not null,
+login_AMI nvarchar(20) not null,
+password_AMI nvarchar(256) not null,
+asterisk_owner uniqueidentifier foreign key references dbo.aspnet_Users(UserId),
+)
+
+
+alter proc insertUniqueAsterisk
+@name_asterisk nvarchar(20),
+@prefix_Asterisk nvarchar(10),
+@ip_address nvarchar(20),
+@login_AMI nvarchar(20),
+@password_AMI nvarchar(256),
+@asterisk_owner nvarchar(256)
+as
+Begin
+	declare @countName int
+	declare @countIP int
+	declare @countPrefix int
+
+	Select @countName = COUNT(name_Asterisk) from Asterisks
+	where [name_Asterisk] = @name_asterisk 
+
+	Select @countIP = COUNT(ip_address) from Asterisks
+	where [ip_address] = @ip_address
+
+	Select @countPrefix = COUNT(prefix_Asterisk)from Asterisks
+	where [prefix_Asterisk] = @prefix_asterisk
+
+	if(@countName = 1)
+		begin
+			select 1 as ReturnCode
+		end
+	else if(@countIP = 1)
+		begin
+			select 2 as ReturnCode
+		end
+	else if(@countPrefix = 1)
+		begin
+			select 3 as ReturnCode
+		end
+	else
+	begin	
+		INSERT INTO Asterisks(name_Asterisk, prefix_Asterisk, ip_address, login_AMI, password_AMI, asterisk_owner)
+                    values(@name_Asterisk, @prefix_Asterisk, @ip_address, @login_AMI, @password_AMI, (SELECT UserId FROM dbo.aspnet_Users WHERE UserName = @asterisk_owner))
+		select -1 as ReturnCode
+	end
+End
+
+alter proc updateAsterisk
+@id_Asterisk int,
+@name_asterisk nvarchar(20),
+@prefix_Asterisk nvarchar(10),
+@ip_address nvarchar(20),
+@login_AMI nvarchar(20),
+@password_AMI nvarchar(256)
+as
+Begin
+	declare @countName int
+	declare @countIP int
+	declare @countPrefix int
+
+	Select @countName = COUNT(name_Asterisk) from Asterisks
+	where [name_Asterisk] = @name_asterisk
+
+	Select @countIP = COUNT(ip_address) from Asterisks
+	where [ip_address] = @ip_address
+
+	Select @countPrefix = COUNT(prefix_Asterisk)from Asterisks
+	where [prefix_Asterisk] = @prefix_asterisk
+
+	if(@countName > 1)
+	begin
+		select 1 as ReturnCode
+	end
+	else if(@countIP > 1)
+		begin
+			select 2 as ReturnCode
+		end
+	else if(@countPrefix > 1)
+		begin
+			select 3 as ReturnCode
+		end
+	else
+	begin	
+		UPDATE Asterisks 
+		SET name_Asterisk = @name_asterisk, prefix_Asterisk = @prefix_Asterisk, ip_address = @ip_address, login_AMI = @login_AMI, password_AMI = @password_AMI
+		WHERE id_Asterisk = @id_Asterisk 
+		select -1 as ReturnCode
+	end
+End
+
+alter proc insertUniqueTrunkByAsterisk
+@trunk_name nvarchar(30),
+@host_ip nvarchar(50),
+@context_name nvarchar(30),
+@id_Asterisk int
+as
+Begin
+	declare @countTrunkName int
+	declare @countHostIP int
+
+	Select @countTrunkName = COUNT(trunk_name), @countHostIP = COUNT(host_ip) from Trunks
+	where [id_Asterisk] = @id_Asterisk and [trunk_name] = @trunk_name or [host_ip] = @host_ip
+
+	if(@countTrunkName > 1 or @countHostIP > 1)
+	begin
+		select 1 as ReturnCode
+	end
+	else
+	begin	
+		INSERT INTO Trunks(trunk_name, host_ip, context_name, id_Asterisk)
+                    values(@trunk_name, @host_ip, @context_name, @id_Asterisk)
+		select -1 as ReturnCode
+	end
+End
+
+alter proc updateTrunk
+@id_trunk int,
+@trunk_name nvarchar(30),
+@host_ip nvarchar(50),
+@context_name nvarchar(30),
+@id_Asterisk int
+as
+Begin
+	declare @countTrunkName int
+
+	Select @countTrunkName = COUNT(trunk_name) from Trunks
+	where [id_Asterisk] = @id_Asterisk and [trunk_name] = @trunk_name 
+
+	if(@countTrunkName > 1)
+	begin
+		select 1 as ReturnCode
+	end
+	else
+	begin	
+		UPDATE Trunks 
+		SET trunk_name = @trunk_name, host_ip = @host_ip, context_name = @context_name
+		WHERE id_trunk = @id_trunk 
+		select -1 as ReturnCode
+	end
+End
+
+
+select * from dbo.aspnet_Users 
+select * from Asterisks
+delete from Asterisks
+delete from Trunks
+drop table Asterisks
+drop table Trunks
+delete from dbo.aspnet_Users
+delete from dbo.aspnet_Membership
+
+select * from Trunks
+
+select COUNT(name_Asterisk), COUNT(ip_address), COUNT(prefix_Asterisk) from Asterisks where name_Asterisk = 'asterisk214' or ip_address = '158.196.244.214' or prefix_Asterisk = '1'
+
+INSERT INTO Trunks(trunk_name, host_ip, context_name, id_Asterisk)
+values('trunk1', '1.1.1.1','context','1')
+
+select * from Asterisks where asterisk_owner = (select UserId from dbo.aspnet_Users where UserName = 'bud0019')
+
+INSERT INTO Asterisks(name_Asterisk, prefix_Asterisk, ip_address, login_AMI, password_AMI, asterisk_owner)
+                    values('asterisk214', '1', '158.196.244.214', 'asterisk214', 'asterisk214', (SELECT UserId FROM dbo.aspnet_Users WHERE UserName = 'bud0019'))
